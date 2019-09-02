08028101ebe78fa6fe83d7f0d6d9e9074a9820f3
Steve Grunwell <steve@stevegrunwell.com>
2019-09-02 19:41:42 +0000

    Rework the user card to remove some noise

--- resources/views/users/card.blade.php
+++ resources/views/users/card.blade.php
@@ -1,8 +1,16 @@
 <article class="card">
-    <div class="card-body">
-        <h3 class="card-title h5">{{ $user->display_name }}</h1>
-        <p class="card-subtitle mb-3">{!! username($user) !!}</p>
+    <div class="card-body media">
+        <img src="{{ $user->getAvatarUrl(100) }}" alt="" class="avatar mr-3">
+        <div class="media-body">
+            <h3 class="card-title h5">{{ $user->display_name }}</h3>
+            <p class="card-subtitle mb-2">{!! username($user) !!}</p>
 
-        @include('users.user-counts', ['user' => $user])
+            @unless (! auth()->check() || $user->id === auth()->user()->id)
+                <follow-button
+                    username="{{ $user->username }}"
+                    :following="{{ auth()->user()->follows($user) ? 'true' : 'false' }}"
+                />
+            @endunless
+        </div>
     </div>
 </article>
