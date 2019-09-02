417bcdd9c7465569531b026d6ebd8b7e618a84a8
Steve Grunwell <steve@stevegrunwell.com>
2019-09-02 23:08:43 +0000

    Show a notice if someone hasn't barked yet

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,19 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-12 sidebar">
-            <article class="card sticky-sidebar mb-4">
-                <div class="card-body">
-                    @include('users.profile', ['user' => $user])
-                </div>
-            </article>
-        </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 sidebar">
+                <article class="card sticky-sidebar mb-4">
+                    <div class="card-body">
+                        @include('users.profile', ['user' => $user])
+                    </div>
+                </article>
+            </div>
 
-        <div class="col">
-            <timeline route="{{ route('api.userTimeline', ['user' => $user]) }}" />
+            <div class="col">
+                @if ($user->post_count)
+                    <timeline route="{{ route('api.userTimeline', ['user' => $user]) }}" />
+                @else
+                    <section class="pt-5 text-center">
+                    <h2 class="h3 text-muted">{{ __('@:username hasn\'t barked yet.', ['username' => $user->username]) }}</h2>
+                    <p class="text-muted">{{ __('When they do, their barks will show up here.') }}</p>
+                @endif
+            </div>
         </div>
     </div>
-</div>
 @endsection
