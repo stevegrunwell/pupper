a03ee7144f4a45bbe580aecadd94f87ff229094b
Steve Grunwell <steve@stevegrunwell.com>
2019-09-03 02:39:13 +0000

    Remove an unnecessary namespace import

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
