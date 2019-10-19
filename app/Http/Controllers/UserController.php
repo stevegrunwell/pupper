da6883a40f9ae0d9f263f6d11efb49ff6dd7dd1f
Steve Grunwell <steve@stevegrunwell.com>
2019-10-19 19:37:40 +0000

    Remove unused namespace import

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\Post as PostResource;
 use App\Scopes\ReverseChronologicalOrderScope;
 use App\User;
 use Illuminate\Contracts\Support\Renderable;
