6fa3535e8d0cac89ff351f390d1fb4827d10705e
Steve Grunwell <steve@stevegrunwell.com>
2019-10-20 19:16:46 +0000

    Remove a regression test (and its fix) so we can do it live

--- tests/Unit/Listeners/InspectNewPostTest.php
+++ tests/Unit/Listeners/InspectNewPostTest.php
@@ -92,23 +92,6 @@ class InspectNewPostTest extends TestCase
     }
 
     /**
-     * @test
-     */
-    public function a_user_should_not_be_notified_about_their_own_posts()
-    {
-        Notification::fake();
-
-        $post = factory(Post::class)->create();
-
-        $this->parser->shouldReceive('getUsernames')
-            ->andReturn([$post->user->username]);
-
-        $this->handle($post);
-
-        Notification::assertNothingSent();
-    }
-
-    /**
      * Invoke the handle() method.
      */
     protected function handle(Post $post = null)
