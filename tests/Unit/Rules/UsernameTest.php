8abf277538a136fdab6ffb831cd67436ab4e3b9e
Steve Grunwell <steve@stevegrunwell.com>
2019-10-22 19:06:02 +0000

    Tests for the username validation rule

--- tests/Unit/Rules/UsernameTest.php
+++ tests/Unit/Rules/UsernameTest.php
@@ -11,8 +11,47 @@ use Tests\TestCase;
 class UsernameTest extends TestCase
 {
     /**
-     * @todo Write some tests in here!
+     * @test
+     * @testWith ["stevegrunwell"]
+     *           ["steve123"]
      */
+    public function it_should_accept_alphanumeric_characters($username)
+    {
+        $this->assertTrue($this->validateUsername($username));
+    }
+
+    public function provide_cool_usernames()
+    {
+        return [
+            'All letters' => ['stevegrunwell'],
+            'Mix of letters and numbers' => ['steve123'],
+            'Start with an underscore' => ['_foo'],
+        ];
+    }
+
+    /**
+     * @test
+     */
+    public function usernames_cannot_begin_with_digits()
+    {
+        $this->assertFalse($this->validateUsername('123foobar'));
+    }
+
+    /**
+     * @test
+     */
+    public function usernames_cannot_be_a_single_character()
+    {
+        $this->assertFalse($this->validateUsername('a'));
+    }
+
+    /**
+     * @test
+     */
+    public function we_should_get_our_custom_validation_message()
+    {
+        $this->assertSame(trans('validation.username'), (new Username)->message());
+    }
 
     /**
      * Shortcut for validating a username against the Username rule.
